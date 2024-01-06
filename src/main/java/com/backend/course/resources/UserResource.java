d5db770956f56f7ec0aa3166c60c7e24cf6d05c9
gscode <adm.guilhermedesousa@gmail.com>
2024-01-06 14:08:19 -0300

    Service layer, component registration

--- src/main/java/com/backend/course/resources/UserResource.java
+++ src/main/java/com/backend/course/resources/UserResource.java
@@ -1,19 +1,36 @@
 package com.backend.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.course.entities.User;
+import com.backend.course.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@email.com", "999999999", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {	
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) { // Para funcionar parâmetros na url deve ter o @PathVariable
+		User obj = service.findByid(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	
+	
 }
