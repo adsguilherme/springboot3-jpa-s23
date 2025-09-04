b71852a3de3647ebc3bda2b5fe4d013027492e55
gscode <adm.guilhermedesousa@gmail.com>
2025-09-04 15:21:58 -0300

    Order, Instant, ISO 8601 - p02

--- src/main/java/com/backend/course/entities/Order.java
+++ src/main/java/com/backend/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.backend.course.entities;
 
+import com.backend.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -18,6 +19,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private OrderStatus orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -25,9 +28,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        this.orderStatus = orderStatus;
         this.client = client;
     }
 
@@ -37,6 +41,14 @@ public class Order implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
     }
 
     public Instant getMoment() {
